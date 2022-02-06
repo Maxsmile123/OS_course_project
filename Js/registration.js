37626d85db6208b3503b70417d7869d56b037247
Maxsmile123 <sysoevmaksim27@gmail.com>
2022-02-06 23:05:22 +0300

    Add auth and registration/

---
+++ Js/registration.js
@@ -0,0 +1,68 @@
+function blink()
+{
+    if(document.querySelector("blink"))
+    {
+        var d = document.querySelector("blink") ;
+        d.style.visibility= (d.style.visibility=='hidden'?'visible':'hidden');
+        setTimeout('blink()', 500);
+    }
+}
+
+$(function(){
+    webSocket = new WebSocket("ws://localhost:9001");
+    webSocket.onmessage = function(event){
+        console.log(event);
+        data = JSON.parse(event.data);
+        processingMSG(data);
+    };
+    let processingMSG = function(data){
+        console.log(data);
+        if (data.command == "registration"){
+            if (data.result == "true"){
+                window.location.href = "C:\\Users\\Sysoe\\VSCodeProjects\\Frontend motherfacker\\index.html";
+            } else if (data.result == "false"){
+                let msgplace = $('.incorrent-login');
+                if(msgplace[0].innerHTML == ""){
+                    let msg = data.reason;
+                    msgplace.append(msg);
+                }
+            }
+        } else{
+            alert("Произошла ошибка, попробуйте ещё раз!");
+        }
+    }
+    function validate (input) {
+        if(input.match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            return false;
+        } else {
+            if(input == ''){
+                return false;
+            }
+        } return true;
+    }
+
+    $("#Register").click(function(){
+        var login = document.getElementById('inputLogin');
+        var name = document.getElementById('inputName');
+        var password = document.getElementById('inputPass');
+        if(!validate(login.value)){
+            login.value = "";
+            name.value = "";
+            password.value = "";
+            return;
+        }
+        let json = {
+            "command": "registration",
+            "name": name.value,
+            "login": login.value,
+            "password": password.value
+        }
+        login.value = "";
+        name.value = "";
+        password.value = "";
+        webSocket.send(JSON.stringify(json));
+    })
+
+
+});
+
